e6f3a249bd5ee7fe79d21f4f10ee846983877468
simkear <46024996+simkear@users.noreply.github.com>
2020-10-05 23:38:31 +0200

    Update app.pp

--- manifests/app.pp
+++ manifests/app.pp
@@ -6,4 +6,9 @@ node 'node3' {
     group         => 'tomcat',
     service_state => 'running'
    }
+   tomcat::deploy {'sysfoo':
+     deploy_url     => 'https://github.com/simkear/sysfoo/raw/master/sysfoodev.war',
+     #checksum_value => '51f44e3d222b4dbb65ceb5647c9b8184',
+    }
 }
+
