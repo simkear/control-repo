a12a9cdfad01bf2db134ad53216ac964e658d074
simkear <46024996+simkear@users.noreply.github.com>
2020-10-05 23:19:01 +0200

    Update app.pp

--- manifests/app.pp
+++ manifests/app.pp
@@ -28,3 +28,11 @@ node 'node2' {
    }
   #include base
 }
+node 'node3' {
+  class {'::tomcat':
+    xms           => '64m',
+    xmx           => '256m',
+    user          => 'tomcat',
+    group         => 'tomcat',
+    service_state => 'running'
+   }
