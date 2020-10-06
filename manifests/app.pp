1850ce970bb137a79f6bc95c20ce663db13696ee
simkear <46024996+simkear@users.noreply.github.com>
2020-10-06 11:24:56 +0200

    Update app.pp

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,4 +1,4 @@
-node 'node1'{
+node 'node2'{
   class {'::tomcat':
     xms           => '64m',
     xmx           => '128m',
@@ -8,23 +8,6 @@ node 'node1'{
    }
    tomcat::deploy {'sysfoo':
      deploy_url     => 'https://github.com/simkear/sysfoo/raw/master/sysfoo.war',
-     checksum_value => '51f44e3d222b4dbb65ceb5647c9b8184',
+     checksum_value => 'd82fa6790d85e545ddc869dbbef8bc57',
     }
  }
-
-
-
-#node 'default'{
-  #notify {"*****DEFAULT******":
-    #message => 'Check point_1'}
-#}
-node 'node2' {
-  class {'::tomcat':
-    xms           => '64m',
-    xmx           => '256m',
-    user          => 'tomcat',
-    group         => 'tomcat',
-    service_state => 'running'
-   }
-  #include base
-}
